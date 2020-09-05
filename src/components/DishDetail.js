3bfc8dfc016a8c4ab7ea834ae3c0507e5245b03e
Sandeep Mandal <sandeepmandal415@gmail.com>
2020-09-05 11:43:55 +0530

    React animation components

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -19,18 +19,26 @@ import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./Loading";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderDish({ dish }) {
   if (dish != null)
     return (
       <div className="col-12 col-md-5 m-1">
-        <Card>
-          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+        <FadeTransform
+          in
+          transformProps={{
+            exitTransform: "scale(0.5) translateY(-50%)",
+          }}
+        >
+          <Card>
+            <CardImg top src={baseUrl + dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+        </FadeTransform>
       </div>
     );
   else return <div></div>;
@@ -178,23 +186,25 @@ function RenderComments({ comments, postComment, dishId }) {
   if (comments != null) {
     const commentsList = comments.map((comment) => {
       return (
-        <div key={comment.id} className="col-12">
-          <p>{comment.comment}</p>
-          <p>
-            -- {comment.author},{" "}
-            {new Intl.DateTimeFormat("en-US", {
-              year: "numeric",
-              month: "short",
-              day: "2-digit",
-            }).format(new Date(Date.parse(comment.date)))}
-          </p>
-        </div>
+        <Fade in>
+          <div key={comment.id} className="col-12">
+            <p>{comment.comment}</p>
+            <p>
+              -- {comment.author},{" "}
+              {new Intl.DateTimeFormat("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "2-digit",
+              }).format(new Date(Date.parse(comment.date)))}
+            </p>
+          </div>
+        </Fade>
       );
     });
     return (
       <div className="m-1">
         <h4>Comments</h4>
-        {commentsList}
+        <Stagger in>{commentsList}</Stagger>
         <CommentForm postComment={postComment} dishId={dishId} />
       </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ import {
   CardBody,
   CardTitle,
   CardSubtitle,
-  CardImgOverlay,
 } from "reactstrap";
 import { Loading } from "./Loading";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform } from "react-animation-components";
 
 function RenderCard({ item, isLoading, errMess }) {
   if (isLoading) {
@@ -18,16 +18,23 @@ function RenderCard({ item, isLoading, errMess }) {
     return <h4>{errMess}</h4>;
   } else
     return (
-      <Card>
-        <CardImg src={baseUrl + item.image} alt={item.name} />
-        <CardBody>
-          <CardTitle>{item.name}</CardTitle>
-          {item.designation ? (
-            <CardSubtitle>{item.designation}</CardSubtitle>
-          ) : null}
-          <CardText>{item.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeTransform
+        in
+        transformProps={{
+          exitTransform: "scale(0.5) translateY(-50%)",
+        }}
+      >
+        <Card>
+          <CardImg src={baseUrl + item.image} alt={item.name} />
+          <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? (
+              <CardSubtitle>{item.designation}</CardSubtitle>
+            ) : null}
+            <CardText>{item.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
     );
 }
 
